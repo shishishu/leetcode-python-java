b25c28a3ec91ad499ecc7d4a1a89385d552c4620
shishishu <zhouxj_xinhua@163.com>
2019-10-07 19:36:00 +0800

    add 220 java

---
+++ java_codes/220/Solution.java
@@ -0,0 +1,25 @@
+import java.security.cert.TrustAnchor;
+import java.util.TreeSet;
+
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return false;
+        }
+
+        int i = 0;
+        TreeSet<Long> values = new TreeSet<>();
+        while (i < nums.length){
+            Long floor = values.floor((long) nums[i]);
+            Long ceil = values.ceiling((long) nums[i]);
+            if ((floor != null && nums[i] - floor <= t) || (ceil != null && ceil - nums[i] <= t)) {
+                return true;
+            }
+            values.add((long) nums[i++]);
+            if (i > k) {
+                values.remove((long) nums[i - k - 1]);
+            }
+        }
+        return false;
+    }
+}
