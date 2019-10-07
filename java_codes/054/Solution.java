ed66efe3d41bff637aa30c49cc5f8e7af8085c4b
shishishu <zhouxj_xinhua@163.com>
2019-10-07 17:36:42 +0800

    054 java

---
+++ java_codes/054/Solution.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (matrix.length == 0) {
+            return res;
+        }
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            // traverse right
+            for (int j = colBegin; j <= colEnd; j++) {
+                res.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+            // traverse down
+            for (int j=rowBegin; j <= rowEnd; j++) {
+                res.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+
+            if (rowBegin > rowEnd || colBegin > colEnd) {
+                break;
+            }
+
+            // traverse left
+            for (int j = colEnd; j >= colBegin; j--) {
+                res.add(matrix[rowEnd][j]);
+            }
+            rowEnd--;
+            // traverse up
+            for (int j = rowEnd; j >= rowBegin; j--) {
+                res.add(matrix[j][colBegin]);
+            }
+            colBegin++;
+        }
+        return res;
+    }
+}
