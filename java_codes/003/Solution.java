6de7082e123f39e71a936763873a703bd90f3465
shishishu <zhouxj_xinhua@163.com>
2019-11-10 22:13:25 +0800

    add 003

---
+++ java_codes/003/Solution.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+// method 1
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int result = 0;
+        int[] cache = new int[256];
+        for (int i = 0, j = 0; i < s.length(); i++){
+            j = (cache[s.charAt(i)] > 0) ? Math.max(j, cache[s.charAt(i)]) : j;
+            cache[s.charAt(i)] = i + 1;
+            result = Math.max(result, i - j + 1);
+        }
+        return result;
+    }
+}
+
+// method 2
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while(i < n & j < n){
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
