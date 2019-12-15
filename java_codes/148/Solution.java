a3996bd91794ff8aaec143e798284be251972815
shishishu <zhouxj_xinhua@163.com>
2019-12-15 21:11:02 +0800

    add 148

---
+++ java_codes/148/Solution.java
@@ -0,0 +1,51 @@
+import java.util.List;
+import java.util.ListIterator;
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+    }
+}
+
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode prev = null, slow = head, fast = head;
+
+        while (fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        return merge(l1, l2);
+    }
+
+    ListNode merge(ListNode l1, ListNode l2){
+        ListNode l = new ListNode(0), p = l;
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                p.next = l1;
+                l1 = l1.next;
+            } else{
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null){
+            p.next = l1;
+        }
+        if (l2 != null){
+            p.next = l2;
+        }
+        return l.next;
+    }
+}
