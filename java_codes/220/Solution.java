c76b03fdd15b6c023d0393fa6d6df85e023c21b7
shishishu <zhouxj_xinhua@163.com>
2020-04-19 17:18:16 +0800

    update 220

--- java_codes/220/Solution.java
+++ java_codes/220/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeSet;
 
 public class Solution {
@@ -20,5 +22,37 @@ public class Solution {
             }
         }
         return false;
+    }
+}
+
+// bucket sorting
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (t < 0) {
+            return false;
+        }
+        Map<Long, Long> d = new HashMap<>();
+        long w = (long)t + 1;
+        for (int i = 0; i < nums.length; i++){
+            long m = getID(nums[i], w);
+            if (d.containsKey(m)){
+                return true;
+            }
+            if (d.containsKey(m-1) && Math.abs(d.get(m-1) - nums[i]) <= t){
+                return true;
+            }
+            if (d.containsKey(m+1) && Math.abs(d.get(m+1) - nums[i]) <= t){
+                return true;
+            }
+            d.put(m, (long)nums[i]);
+            if (i >= k){
+                d.remove(getID(nums[i-k], w));
+            }
+        }
+        return false;
+    }
+
+    private long getID(long i, long w){
+        return i < 0 ? (i + 1) / w - 1: i / w;
     }
 }
