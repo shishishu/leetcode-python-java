719b39ed6d3425d2103464be798f31273dfaa204
shishishu <zhouxj_xinhua@163.com>
2020-04-19 20:33:44 +0800

    add 540

---
+++ java_codes/540/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high){
+            int mid = (low + high) / 2;
+            if (nums[mid] == nums[mid ^ 1]){
+                low = mid + 1;
+            } else{
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
+
+// variation
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high){
+            int mid = (low + high) / 2;
+            if (mid % 2 == 1){
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]){
+                low = mid + 2;
+            } else{
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
